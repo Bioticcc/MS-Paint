1bdaf9303afa61471880c8c0e4ecce98a58f5ec3
Bio <144750331+Bioticcc@users.noreply.github.com>
2024-11-17 16:52:02 -0800

    Drawing to screen works!
    bababooey!

--- MS-Paint/Game.h
+++ MS-Paint/Game.h
@@ -17,6 +17,9 @@ private:
 
 	sf::Clock clock;
 	sf::Time deltaTime;
+	sf::CircleShape ink;
+
+	std::vector<sf::CircleShape> inkDots;
 
 public:
 
@@ -27,19 +30,48 @@ public:
 		background.setTexture(backgroundT);
 	}
 
-	void runGame() {
-		while (window.isOpen()) {
-			deltaTime = clock.restart();
-			while (window.pollEvent(event)) {
-				if (event.type == sf::Event::Closed) {
-					window.close();
-				}
-			}
-			window.clear();
-			window.draw(background);
-			window.display();
-		}
-	}
+
+    void runGame() {
+        int i = 0;
+
+        while (window.isOpen()) {
+            deltaTime = clock.restart();
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed) {
+                    window.close();
+                }
+            }
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                // "spawns" a pixel object every frame (drawing pixel object to screen)
+                float size = 5;
+                sf::CircleShape ink(size);
+                ink.setFillColor(sf::Color::Green);
+                sf::Vector2i cursorPos = sf::Mouse::getPosition(window);
+                ink.setPosition(static_cast<float>(cursorPos.x), static_cast<float>(cursorPos.y));
+                inkDots.push_back(ink); // Store the ink object in the vector
+                i++;
+                std::cout << "ink#" << i << " pos: " << ink.getPosition().x << "," << ink.getPosition().y << std::endl;
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::E)) {
+                inkDots.clear(); // Clear the vector of ink objects
+                window.clear();
+                i = 0;
+                system("cls");
+            }
+
+           // window.clear(); // Clear the window before drawing
+            window.draw(background); // Draw the background
+
+            // Draw all the ink objects
+            for (const auto& ink : inkDots) {
+                window.draw(ink);
+            }
+
+            window.display(); // Display the drawn frame
+        }
+    }
 
 
 
